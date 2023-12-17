2160ba1ce16bdf6f2b14a394148c9ab6f04e4890
Richlogs <richcostello@outlook.co.nz>
2023-12-17 22:16:44 +1300

    chore: add move file function

--- podcasts/main.py
+++ podcasts/main.py
@@ -2,14 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 
 import customtkinter
+import utils.gui as gui
 from init.loging import get_logger
-from utils.gui import get_selected_rows, select_data, select_folder, set_tk_style
 from utils.sqlite import get_podcast_data
 
 logger = get_logger()
 
 df = get_podcast_data()
-df = select_data(df)
+df = gui.select_data(df)
 
 
 customtkinter.set_appearance_mode("dark")
@@ -20,7 +20,7 @@ frame = customtkinter.CTkFrame(root)
 frame.pack(pady=10, padx=20, fill="both", expand=True)
 
 
-set_tk_style()
+gui.set_tk_style()
 tree = ttk.Treeview(frame, columns=list(df.columns), show="headings")
 
 # Defining column headings
@@ -34,11 +34,14 @@ for _, row in df.iterrows():
 tree.pack(expand=True, fill="both")
 
 # Button to get selected rows
-select_button = tk.Button(frame, text="Get Selected Rows", command=get_selected_rows)
+
+select_button = tk.Button(
+    frame, text="Get Selected Rows", command=lambda: gui.get_selected_rows(tree)
+)
 select_button.pack()
 
 
-folder_button = tk.Button(frame, text="Select Folder", command=select_folder)
+folder_button = tk.Button(frame, text="Select Folder", command=gui.select_folder)
 folder_button.pack()
 
 # Label to display selected folder path
